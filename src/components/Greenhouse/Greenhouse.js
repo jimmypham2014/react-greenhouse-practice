e8dfd9f32791bcf6609d88103279401343348e62
geoffreyjiang <geoffreychjiang@gmail.com>
2022-10-05 20:16:57 -0700

    phase 2 done

--- src/components/Greenhouse/Greenhouse.js
+++ src/components/Greenhouse/Greenhouse.js
@@ -1,18 +1,29 @@
-import dayImage from './images/greenhouse-day.jpg';
-import nightImage from './images/greenhouse-night.jpg';
-import './Greenhouse.css';
-
-import LightSwitch from './LightSwitch';
-import ClimateStats from './ClimateStats';
+import dayImage from "./images/greenhouse-day.jpg";
+import nightImage from "./images/greenhouse-night.jpg";
+import "./Greenhouse.css";
+import { ThemeContext, useTheme } from "../../context/ThemeContext";
+import LightSwitch from "./LightSwitch";
+import ClimateStats from "./ClimateStats";
+import { useState, useEffect } from "react";
 
 function Greenhouse() {
+  const { themeName, setThemeName } = useTheme();
+  // const [img, setImg] = useState(nightImage);
+  // console.log(themeName);
+  // useEffect(() => {
+  //   if (themeName === "day") {
+  //     setImg(dayImage);
+  //   } else if (themeName === "night") {
+  //     setImg(nightImage);
+  //   }
+
+  // }, [themeName, img]);
+  let img;
+  img = themeName === "day" ? dayImage : nightImage;
 
   return (
     <section>
-      <img  className='greenhouse-img'
-            src={dayImage}
-            alt='greenhouse' 
-      />
+      <img className="greenhouse-img" src={img} alt="greenhouse" />
       <LightSwitch />
       <ClimateStats />
     </section>
--- src/components/Greenhouse/LightSwitch.js
+++ src/components/Greenhouse/LightSwitch.js
@@ -1,10 +1,33 @@
-import './LightSwitch.css';
-
+import "./LightSwitch.css";
+import { ThemeContext, useTheme } from "../../context/ThemeContext";
+import dayImage from "./images/greenhouse-day.jpg";
+import nightImage from "./images/greenhouse-night.jpg";
+import { useState } from "react";
 function LightSwitch() {
+  const { themeName, setThemeName } = useTheme();
+  const [img, setImg] = useState();
+  // console.log(themeName, "lightswitch");
+
   return (
-    <div className="light-switch day">
-      <div className="on">DAY</div>
-      <div className="off">NIGHT</div>
+    <div className={`light-switch ${themeName}`}>
+      <div
+        onClick={() => {
+          setImg(dayImage);
+          setThemeName("day");
+        }}
+        className="on"
+      >
+        DAY
+      </div>
+      <div
+        onClick={() => {
+          setImg(nightImage);
+          setThemeName("night");
+        }}
+        className="off"
+      >
+        NIGHT
+      </div>
     </div>
   );
 }
