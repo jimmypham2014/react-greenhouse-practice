c4b865f2a075af9c655389d4c4b93a137e8de25b
jimmy <jimmyfam96@gmail.com>
2022-10-05 20:50:21 -0700

    finished Hygro

--- src/context/ClimateContext.js
+++ src/context/ClimateContext.js
@@ -1,2 +1,29 @@
 // Temperature has a default value of 50 degrees
 // Humidity has a default value of 40%
+
+import { createContext, useContext, useState } from "react";
+import {Thermometer} from '../components/Thermometer/Thermometer'
+
+
+export  const ClimateContext = createContext()
+
+export const useClimate = () => useContext(ClimateContext)
+
+export default function ClimateProvider({children}) {
+    const [temperature, setTemperature] = useState(50)
+    const [humidity,setHumidity] = useState(40)
+  return (
+    <ClimateContext.Provider
+     value ={{
+        temperature,
+        setTemperature,
+        humidity,
+        setHumidity
+     }}
+    
+    >
+        
+    {children}
+    </ClimateContext.Provider>
+  )
+}
