a9bdb3897d09caa3ccb88c6a4481daa3d7185267
jimmy <jimmyfam96@gmail.com>
2022-10-05 21:55:35 -0700

    sdfds

--- src/components/Hygrometer/Hygrometer.js
+++ src/components/Hygrometer/Hygrometer.js
@@ -1,14 +1,45 @@
 import ReactSlider from "react-slider";
 import "./Hygrometer.css";
-import { useClimate } from "../../context/ClimateContext";
+import { useClimate} from "../../context/ClimateContext";
+import{useEffect,useState } from 'react'
 
 function Hygrometer() {
   const { humidity, setHumidity } = useClimate();
+  const [desiredHumidity, setDesiredHumidity] = useState(humidity);
+
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+       if(humidity > desiredHumidity && humidity-desiredHumidity ===1){
+        setDesiredHumidity((preTemp) => {
+          return preTemp + 1;
+        });
+       }
+      else if (humidity > desiredHumidity) {
+        setDesiredHumidity((preTemp) => {
+          return preTemp + 2;
+        });
+
+      } else if(humidity < desiredHumidity && desiredHumidity-humidity ===1){
+          setDesiredHumidity((preTemp) => {
+            return preTemp - 1;
+          });
+        
+      } else if (humidity < desiredHumidity) {
+        setDesiredHumidity((preTemp) => {
+          return preTemp - 2;
+        })
+      }
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, [humidity, desiredHumidity]);
+
+
 
   return (
     <section>
       <h2>Hygrometer</h2>
-      <div className="actual-humid">Actual Humidity: {humidity}%</div>
+      <div className="actual-humid">Actual Humidity: {desiredHumidity}%</div>
       <ReactSlider
         value={humidity}
         onAfterChange={(val) => setHumidity(val)}
